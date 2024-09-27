8053055b3471a51bcf8a76e1189b25402701e02c
Matilde Morrone <contact@morrone.dev>
2024-09-27 18:30:18 +0200

    Create basic cmdline interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::{builder::Styles, Parser};
+use clap_cargo::style;
+
+pub const CARGO_STYLE: Styles = Styles::styled()
+    .header(style::HEADER)
+    .usage(style::USAGE)
+    .literal(style::LITERAL)
+    .placeholder(style::PLACEHOLDER)
+    .error(style::ERROR)
+    .valid(style::VALID)
+    .invalid(style::INVALID);
+
+#[derive(Parser)]
+#[command(name = "cargo")]
+#[command(bin_name = "cargo")]
+#[command(styles = CARGO_STYLE)]
+enum CargoCli {
+    Pacco(PaccoArgs),
+}
+
+#[derive(clap::Args)]
+#[command(version, about, long_about = None)]
+struct PaccoArgs {}
+
 fn main() {
-    println!("Hello, world!");
+    let CargoCli::Pacco(_args) = CargoCli::parse();
 }
